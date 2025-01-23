4509cd49c104654a8c8585edfb96a1480ef02e4c
Shae <skitchen@mail.smu.edu>
2025-01-23 17:43:32 -0600

    directories push

--- JAVA/index.js
+++
@@ -1,7 +0,0 @@
-document
-  .querySelector("#GreatGooglyMoogly")
-  .addEventListener(click, handleClick);
-
-function handleClick() {
-  console.log("Big Balls of Fire");
-}
---
+++ public/CSS/JAVA/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(() => {
+  document.querySelector("testButton").addEventListener("click", handleClick);
+
+  function handleClick() {
+    console.log("Big Balls of Fire");
+  }
+})();

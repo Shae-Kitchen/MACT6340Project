20bcbc76482cafbafd4e96d51bdbc322edf6753c
Shae <skitchen@mail.smu.edu>
2025-01-30 13:19:10 -0600

    stylized the index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,17 @@
 (function () {
   "use strict";
 
-  document
-    .querySelector("contact-form-submit")
-    .addEventListener("click", contactSubmit);
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".card-cover").forEach((card) => {
+      card.addEventListener("mouseenter", () => {
+        card.querySelector(".title").style.display = "none";
+        card.querySelector(".description").style.display = "block";
+      });
 
-  function contactSubmit() {
-    console.log("Thanks! We will be reaching out soon.");
-  }
+      card.addEventListener("mouseleave", () => {
+        card.querySelector(".title").style.display = "block";
+        card.querySelector(".description").style.display = "none";
+      });
+    });
+  });
 })();

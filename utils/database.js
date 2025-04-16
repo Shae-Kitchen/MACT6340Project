4064c358e9b509e838b7fb78a8e3c03ee8085d73
Shae <skitchen@mail.smu.edu>
2025-04-16 16:07:06 -0500

    Updated database with database and moved over to DO

--- utils/database.js
+++ utils/database.js
@@ -5,22 +5,22 @@ dotenv.config();
 let pool;
 
 export async function connect() {
-  // let cString =
-  // "mysql://" + process.env.MYSQL_USER + ":" + process.env.MYSQL_PASSWORD;
-  //"@" + process.env.MYSQL_HOST;
+  let cString =
+    "mysql://" + process.env.MYSQL_USER + ":" + process.env.MYSQL_PASSWORD;
+  "@" + process.env.MYSQL_HOST;
 
-  //  ":" + process.env.MYSQL_PORT;
+  ":" + process.env.MYSQL_PORT;
 
-  //  "/" + process.env.MYSQL_DATABASE;
+  "/" + process.env.MYSQL_DATABASE;
   pool = mysql
     .createPool(
-      // cString  //digital ocean sql server
-      {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-      }
+      cString //digital ocean sql server
+      // {
+      //   host: process.env.MYSQL_HOST,
+      //   user: process.env.MYSQL_USER,
+      //   password: process.env.MYSQL_PASSWORD,
+      //   database: process.env.MYSQL_DATABASE,
+      // }
     )
     .promise();
   console.log("Database connected!");
